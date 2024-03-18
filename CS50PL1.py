d8eaf1620ec47c96d686743b6b341d23afb8c378
Ananya Mishra <ananya.cr7fan@gmail.com>
2024-03-18 20:24:40 +0530

    smooth operatooooooorrrrrrrrrr

--- CS50PL1.py
+++ CS50PL1.py
@@ -1,16 +1,21 @@
 
 
-# input("what's your name")
-# print("hello, world")
+input("what's your name")
+print("hello, world")
 
 '''
 return value feature in talk
 what will you return if you don't have a feature to hold it ? Presenting you THE VARIABLES *clapclapclappclapclap *heroicentryofvariable
 '''
-
+'''
 name= input("what's your name") #name is the  variable that stores user's input. 
 print("hello,")
 print(name)                     #return value
-
+'''
 
 #next comes PSEUDO CODE , it'sbasically structuring  of code without actual implementation breaking bigger  tasks into smaller ones.
+
+#Let's prettify the code above. 
+
+name = input ("what's your name? ")
+print("hello, " + name + "!")   #we can use the '+' operator to concatenate strings</s>
