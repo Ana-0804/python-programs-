bea55587a40b37a99ef8b60d2fde160f28d539a6
Ananya Mishra <ananya.cr7fan@gmail.com>
2024-03-22 21:18:50 +0530

    We begin with functions

--- Functions.py
+++ Functions.py
@@ -1,7 +1,7 @@
 '''
 If and when you want to define, invent, create your own function yu use #def keyword'''
 
-def hello( to):                             #Here, in the first lines, you are creating your hello function. This time, however, you are telling the compiler that this function takes a single parameter: a variable called to. Therefore, when you call hello(name) the computer passes name into the hello function as to. This is how we pass values into functions. Very useful!
+def hello( to):
     print("hello" , to)
 
 name = input("What's your name ? ")
