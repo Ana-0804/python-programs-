94da16b6005165314ba3313e73543b725a4e1c8a
Ananya Mishra <ananya.cr7fan@gmail.com>
2024-03-24 23:12:32 +0530

    Define main

--- Functions.py
+++ Functions.py
@@ -1,5 +1,5 @@
 '''
-If and when you want to define, invent, create your own function yu use #def keyword'''
+If and when you want to define, invent, create your own function yu use #def keyword
 
 def hello( to):
     print("hello" , to)
@@ -12,3 +12,21 @@ hello(name)
 #This time, however, we are telling the compiler that this function takes a single parameter: a variable called "to". 
 #Therefore, when we call hello(name) the computer passes name into the hello function as to. 
 #This is how we pass values into functions.
+'''
+
+def main():
+
+    # Output using our own function
+    name = input("What's your name? ")
+    hello(name)
+
+    # Output without passing the expected arguments
+    hello()
+
+
+# Create our own function
+def hello(to="world"):
+    print("hello,", to)
+
+
+main()
