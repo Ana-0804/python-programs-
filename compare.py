f65369fa3632fe4129ee28f31d39d8f7f252a5b0
Ananya Mishra <ananya.cr7fan@gmail.com>
2024-04-23 16:33:29 +0530

    if elif

---
+++ compare.py
@@ -0,0 +1,10 @@
+x = int(input("what's x?"))
+y = int(input("what's y? "))
+
+if x>y:
+    print("Y is smaller")
+elif x<y:
+    print("Xis smaller than y")
+elif x==y:
+    print("Both are equal")
+    
