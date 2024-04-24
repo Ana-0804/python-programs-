8483f852300719ac46682ee3338c77bcbeec5518
Ananya Mishra <ananya.cr7fan@gmail.com>
2024-04-24 12:41:48 +0530

    or operator

--- compare.py
+++ compare.py
@@ -1,3 +1,4 @@
+'''
 x = int(input("what's x?"))
 y = int(input("what's y? "))
 
@@ -7,4 +8,14 @@ elif x<y:
     print("Xis smaller than y")
 elif x==y:
     print("Both are equal")
-    
+    '''
+
+                                                #OR
+
+x = int(input("what's x?"))
+y = int(input("what's y? "))
+
+if x>y or y>x:  #this means if either of the conditions in () is true then it will execute this block.
+    print ("values are not equal")
+else:
+    print("both are equal")
